ec4339e5a77f748c5fa5dcd3bbbaf931df5187ac
Wahid Shafique <wahidshafique42@gmail.com>
2023-02-05 11:46:31 -0500

    compress non critical imgs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.css';
-	import bgImage from '$lib/assets/casey-horner-RmoWqDCqN2E-unsplash.jpg';
+	import bgImage from '$lib/assets/casey-horner-RmoWqDCqN2E-unsplash.webp';
 </script>
 
 <div class="fixed -top-[50%] -left-[50%] w-[200%] h-[200%]">
