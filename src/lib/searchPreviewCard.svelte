8b753c58fca1e7b70cac1332279c7b997694ea10
Wahid Shafique <wahidshafique42@gmail.com>
2023-07-15 11:41:26 -0400

    change btns to links

--- src/lib/searchPreviewCard.svelte
+++ src/lib/searchPreviewCard.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
+	import { beforeNavigate } from '$app/navigation';
 	import searchResultCookie from './helpers/searchResultCookie';
 	import notFoundImage from '$lib/assets/404-tribble.webp';
 	import type { FilteredSearchResult } from './types';
 	export let result: FilteredSearchResult;
 	// remove the extra type detail visually, sometimes this card appears in other contexts where type is not needed
 	export let hideMediaType: boolean = false;
+	const nextPage = '/search/' + result.id + `?${result.type}`;
 
-	const handleClick = () => {
+	beforeNavigate(({ to }) => {
 		// store the details of our results so we do not need to make another request. Only relevant when you search for people
 		/** cookie is just for the server to know that we are in sveltekit client mode and have stored data as we navigate forward*/
 		searchResultCookie.set(result);
-		goto('/search/' + result.id + `?${result.type}`);
-	};
+	});
 </script>
 
-<button on:click={handleClick} class="group px-10">
+<a href={nextPage} class="group px-10 no-underline">
 	<div
 		class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 relative"
 	>
@@ -43,4 +43,4 @@
 	{#if !hideMediaType}
 		<p class="mt-1 mb-3 text-xs font-medium">{result.type}</p>
 	{/if}
-</button>
+</a>
